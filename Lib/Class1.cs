3a328ccce0f5b199c3e485db23000f0287889ae1
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2022-11-07 08:55:33 +0300

    1

--- Lib/Class1.cs
+++ Lib/Class1.cs
@@ -4,12 +4,12 @@ namespace Lib
 {
     public static class IoC2
     {
-        private static ThreadLocal<ConcurrentDictionary<string, string>> IoC = new();
+        private static ThreadLocal<Dictionary<string, string>> IoC = new();
 
         public static void Register(string key, string value)
         {
             if (IoC.Value == null)
-                IoC.Value = new ConcurrentDictionary<string, string>();
+                IoC.Value = new Dictionary<string, string>();
 
             IoC.Value.TryAdd(key, value);
         }
