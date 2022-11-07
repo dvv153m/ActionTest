4005dff1799dd1b8c4ca5e02959aea30f2d41990
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2022-11-07 08:53:45 +0300

    add new test

---
+++ Lib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+
+namespace Lib
+{
+    public static class IoC2
+    {
+        private static ThreadLocal<ConcurrentDictionary<string, string>> IoC = new();
+
+        public static void Register(string key, string value)
+        {
+            if (IoC.Value == null)
+                IoC.Value = new ConcurrentDictionary<string, string>();
+
+            IoC.Value.TryAdd(key, value);
+        }
+
+        public static string Resolve(string key)
+        {
+            var res = IoC.Value.TryGetValue(key, out string value);
+
+            return value;
+        }
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,8 +1,32 @@
+using Lib;
+
 namespace TestProject1
 {
     public class UnitTest1
     {
         [Fact]
+        public void Test2()
+        {
+            var t1 = Task.Factory.StartNew(() => {
+
+                IoC2.Register("a", "A");
+                Thread.Sleep(2000);
+                IoC2.Resolve("a");
+            });
+
+            var t2 = Task.Factory.StartNew(() => {
+
+                IoC2.Register("a", "A");
+                Thread.Sleep(1000);
+                IoC2.Resolve("b");
+            });
+
+            Task.WaitAll(t1, t2);
+
+            IoC2.Register("c", "C");
+        }
+
+            [Fact]
         public void Test1()
         {
             var t1 = 0;
